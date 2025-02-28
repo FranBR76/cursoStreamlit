64dbabbac7c49cab5f717327f3f847f3aed6f8e0
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-02-28 20:41:33 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -4,9 +4,9 @@ st.title("Seja bem-vindo novo aluno")
 
 col1, col2 = st.columns(2,  vertical_alignment="top", gap="large")
 
-
+st.header("Informe seus dados")
 with col1:
-  st.header("Informe seus dados")
+  
   st.text_input("Nome")
   st.text_input("Sobrenome")
   st.text_input("CPF")
