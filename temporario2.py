532d22dc3f07a6a77e64737a304dc0733eea0ff2
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-02-28 20:43:23 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -3,7 +3,7 @@ import datetime
 st.title("Seja bem-vindo novo aluno")
 st.header("Informe seus dados")
 col1, col2 = st.columns(2,  vertical_alignment="top", gap="large")
-col1, col2 = st.columns([2, 2])
+col1, col2 = st.columns([1, 2])
 
 with col1:
   
