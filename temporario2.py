baa6869326b1372261a20a387fa002dd4b9b25d1
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-02-28 20:26:16 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -15,14 +15,14 @@ with col1:
   jan_1 = datetime.date(next_year, 1, 1)
   dec_31 = datetime.date(next_year, 12, 31)
 
-  d = st.date_input(
+  st.date_input(
     "Select your vacation for next year",
     (jan_1, datetime.date(next_year, 1, 7)),
     jan_1,
     dec_31,
     format="MM.DD.YYYY",
   )
-  d
+  
 
 
 with col2:
