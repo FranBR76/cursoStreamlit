3e6dc5befdbd7a3aa9de51211f09669797ce2ed7
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-02-28 20:17:02 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("Seja bem-vindo novo aluno")
 
-col1, col2 = st.columns(2, vertical_alignment="botton", gap="medium")
+col1, col2 = st.columns(2,  vertical_alignment="bottom")
 
 
 with col1:
