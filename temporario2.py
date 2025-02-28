bbf996148ee7ac5fa21b7cd4db9376a1e6dcb2a0
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-02-28 20:24:42 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -9,12 +9,7 @@ with col1:
   st.header("Informe seus dados")
   st.text_input("Nome")
   st.text_input("Sobrenome")
-  # numero = st.text_input("CPF")
-  # numero =   
-  # today = datetime.datetime.now()
-  # next_year = today.year + 1
-  # jan_1 = datetime.date(next_year, 1, 1)
-  # dec_31 = datetime.date(next_year, 12, 31)
+ 
   st.date_input(
     "Select your vacation for next year",
     (jan_1, datetime.date(next_year, 1, 7)),
