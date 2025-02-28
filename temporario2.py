c70b8e461f0f1ac2dd693db9445caba3e0207f16
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-02-28 20:08:04 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
-
-st.header("TESTE")
+col1, col2 = st.columns(2)
+st.header("Seja bem-vindo novo aluno")
+with col1:
+  st.title("Informe seus dados")
+  title = st.text_input("Nome")
