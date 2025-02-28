3868c47829e73f5a967e33d36669147c71a8d01b
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-02-28 19:58:50 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,20 +1,4 @@
 import streamlit as st
 
-col1, col2, col3, col4 = st.columns(4)
 
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
-
-with col4:
-    col4.gap("large")
-    st.header("Testando")
-    st.write("TESTE")
+st.header("TESTE")
