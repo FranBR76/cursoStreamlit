e1172b2bc687e82f4c3bb28c2e6fd331419dbe54
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-02-28 20:18:22 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,15 +1,15 @@
 import streamlit as st
 st.title("Seja bem-vindo novo aluno")
 
-col1, col2 = st.columns(2,  vertical_alignment="bottom")
+col1, col2 = st.columns(2,  vertical_alignment="bottom", gap="large")
 
 
 with col1:
   st.header("Informe seus dados")
   st.text_input("Nome")
   st.text_input("Sobrenome")
-  st.text_input("")
-
+  # numero = st.text_input("CPF")
+  # numero = 
 
 with col2:
   st.text_input("CEP")
