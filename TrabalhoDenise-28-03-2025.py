f2e09bffd4d21c615d1500b1450a79d51adb4780
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-03-28 20:17:33 -0300

    Update TrabalhoDenise-28-03-2025.py

--- TrabalhoDenise-28-03-2025.py
+++ TrabalhoDenise-28-03-2025.py
@@ -1,13 +1,11 @@
+import datetime
 import streamlit as st
 
 def infoUser():
   st.header("Informe seus Dados")
   nome = st.text_input("Informe seu nome ")
   sobrenome = st.text_input("Infome o sobrenome ")
-  dt_nasc = st.date_input(
-    "Informe sua data de nascimento",
-    format="DD.MM.YYYY,
-    )
+  dt_nasc = st.date_input("Informe sua data de nascimento", format="DD.MM.YYYY)
   estadoCivil = st.selectbox(
     "Informe seu estado civil",
     (Solteiro, Casado, Divorciado, Viúvo),
