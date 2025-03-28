2ab0bae52c9f467912f82fe0aa4f39c5e9451558
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-03-28 20:22:29 -0300

    Update TrabalhoDenise-28-03-2025.py

--- TrabalhoDenise-28-03-2025.py
+++ TrabalhoDenise-28-03-2025.py
@@ -5,14 +5,14 @@ def infoUser():
   st.header("Informe seus Dados")
   nome = st.text_input("Informe seu nome ")
   sobrenome = st.text_input("Infome o sobrenome ")
-  # dt_nasc = st.date_input("Informe sua data de nascimento", format="DD.MM.YYYY")
-  d = st.date_input(
-    "Select your vacation for next year",
-    (jan_1, datetime.date(next_year, 1, 7)),
-    jan_1,
-    dec_31,
-    format="MM.DD.YYYY",
-)
+  dt_nasc = st.text_input("Informe sua data de nascimento (DD.MM.YYYY)")
+#   d = st.date_input(
+#     "Select your vacation for next year",
+#     (jan_1, datetime.date(next_year, 1, 7)),
+#     jan_1,
+#     dec_31,
+#     format="MM.DD.YYYY",
+# )
 
   estadoCivil = st.selectbox(
     "Informe seu estado civil",
