4d3ef1ee20a7eb32a53f35fc03fec94bf9094c35
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-03-28 20:15:19 -0300

    Update TrabalhoDenise-28-03-2025.py

--- TrabalhoDenise-28-03-2025.py
+++ TrabalhoDenise-28-03-2025.py
@@ -1,1 +1,24 @@
+import streamlit as st
 
+def infoUser():
+  st.header("Informe seus Dados")
+  nome = st.text_input("Informe seu nome ")
+  sobrenome = st.text_input("Infome o sobrenome ")
+  dt_nasc = st.date_input(
+    "Informe sua data de nascimento",
+    format="DD.MM.YYYY,
+    )
+  estadoCivil = st.selectbox(
+    "Informe seu estado civil",
+    (Solteiro, Casado, Divorciado, Viúvo),
+  )
+  sexo = st.selectbox(
+    "Informe seu sexo",
+    (Masculino, Feminino, Outro),
+  #   if Outro:
+  #     sexo = st.text_input()
+  )
+
+  salario = st.float_input("Informe seu Salário")
+
+  st.write("Dados Informados: ")
