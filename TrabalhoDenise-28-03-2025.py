7333da67b87ae7efcffd07a2d864317e261e842e
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-03-28 20:24:22 -0300

    Update TrabalhoDenise-28-03-2025.py

--- TrabalhoDenise-28-03-2025.py
+++ TrabalhoDenise-28-03-2025.py
@@ -6,13 +6,7 @@ def infoUser():
   nome = st.text_input("Informe seu nome ")
   sobrenome = st.text_input("Infome o sobrenome ")
   dt_nasc = st.text_input("Informe sua data de nascimento (DD.MM.YYYY)")
-#   d = st.date_input(
-#     "Select your vacation for next year",
-#     (jan_1, datetime.date(next_year, 1, 7)),
-#     jan_1,
-#     dec_31,
-#     format="MM.DD.YYYY",
-# )
+
 
   estadoCivil = st.selectbox(
     "Informe seu estado civil",
@@ -21,8 +15,6 @@ def infoUser():
   sexo = st.selectbox(
     "Informe seu sexo",
     (Masculino, Feminino, Outro),
-  #   if Outro:
-  #     sexo = st.text_input()
   )
 
   salario = st.float_input("Informe seu Salário")
