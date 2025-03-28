3f65bf09b7e1a288e285a5c2bfc3bcc83eb93b3e
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-03-28 20:31:50 -0300

    Update trabDenise.py

--- trabDenise.py
+++ trabDenise.py
@@ -4,16 +4,16 @@ def infoUser():
   st.header("Informe seus Dados")
   nome = st.text_input("Informe seu nome ")
   sobrenome = st.text_input("Infome o sobrenome ")
-  dt_nasc = st.text_input("Informe sua data de nascimento (DD.MM.YYYY)")
+  dt_nasc = st.text_input("Informe sua data de nascimento", format="DD.MM.YYYY")
 
 
   estadoCivil = st.selectbox(
     "Informe seu estado civil",
-    (Solteiro, Casado, Divorciado, Viúvo),
+    ("Solteiro", "Casado", "Divorciado", "Viúvo"),
   )
   sexo = st.selectbox(
     "Informe seu sexo",
-    (Masculino, Feminino, Outro),
+    ("Masculino", "Feminino", "Outro"),
   )
 
   salario = st.float_input("Informe seu Salário")
