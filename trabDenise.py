a1101b9fc59756adc05b79b372118d5c7221ab61
Francesco Matteo <42424185+FranBR76@users.noreply.github.com>
2025-04-02 19:14:53 -0300

    Update trabDenise.py

--- trabDenise.py
+++ trabDenise.py
@@ -24,7 +24,7 @@ def infoUser():
       st.write("Dados Informados: ")
       st.write("Nome: ", nome)
       st.write("Sobrenome: ", sobrenome)
-      st.write("Data de Nascimento: ", dt_nasc)
+      st.write("Data de Nascimento: ", dt_nasc.format="DD/MM/YYYY")
       st.write("Estado Civil: ", estadoCivil)
       st.write("Sexo: ", sexo)
       st.write("Salário: ", salario)
